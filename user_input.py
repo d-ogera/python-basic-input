49065fc38ae849711e5eeab9b5c42e1dbe19a28d
Ogera <107644200+d-ogera@users.noreply.github.com>
2024-08-21 10:50:50 +0300

    jghdfh

--- user_input.py
+++ user_input.py
@@ -2,3 +2,4 @@ name = input("What is your name? ")
 age = input("What is your age? ")
 location = input("Where do you live? ")
 print(f"Hello {name}, you are {age} years old and live in {location}.")
+
