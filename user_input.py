6dec23c2eb5baf79285eef6ab35ae52dd525cfae
Ogera <107644200+d-ogera@users.noreply.github.com>
2024-08-27 01:35:10 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,5 +1,6 @@
-name = input("What is your name? ")
+firstName = input("What is your name? ")
+lastName = input("What is your lastname ?")
 age = input("What is your age? ")
-location = input("Where do you live? ")
-print(f"Hello {name}, you are {age} years old and live in {location}.")
+experience = input("eWhat is your experience with aviator?")
+print(f"Hello {firstName} {lastName}, you are {age} years old you mean that you had {experience} experience with aviator!")
 
